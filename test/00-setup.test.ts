856b028a4063f2ac469617b5880151f64b1b1d8c
Pier Fumagalli <pier@usrz.com>
2023-08-31 17:27:03 +0200

    Kill leftover connections in test

--- test/00-setup.test.ts
+++ test/00-setup.test.ts
@@ -1,6 +1,6 @@
 import { randomUUID } from 'node:crypto'
 
-import { $ylw } from '@plugjs/build'
+import { $gry, $ylw } from '@plugjs/build'
 import pg from 'pg'
 
 export const databaseName = `test-${randomUUID()}`
@@ -9,12 +9,37 @@ afterAll(async () => {
   log.notice(`\nDropping database ${$ylw(databaseName)}`)
   const mainClient = new pg.Client({ database: 'postgres' })
   await mainClient.connect()
+
+  let leftovers = 0
   try {
+    // do we have any leftover connections?
+    const result = await mainClient.query(`
+      SELECT application_name
+        FROM pg_stat_activity
+       WHERE application_name LIKE 'pool:%'
+         AND datname=$1`, [ databaseName ])
+
+    if (result.rows.length) {
+      log.warn('Found', $ylw(`${result.rows.length}`), 'connections left open:')
+      result.rows.forEach(({ application_name: name }) => log.warn($gry('  -'), $ylw(name)))
+      leftovers = result.rows.length
+    }
+
+    // kill all remaining connections
+    await mainClient.query(`
+      SELECT pg_terminate_backend(pid)
+        FROM pg_stat_activity
+       WHERE application_name LIKE 'pool:%'
+         AND pid <> pg_backend_pid()
+         AND datname = $1`, [ databaseName ])
+
     await mainClient.query(`DROP DATABASE "${databaseName}"`)
   } finally {
     await mainClient.end()
   }
-})
+
+  expect(leftovers, `Left ${leftovers} connections`).toStrictlyEqual(0)
+}, 30_000)
 
 beforeAll(async () => {
   log.notice(`Creating database ${$ylw(databaseName)}`)
