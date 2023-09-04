4f2363717bd841fd3670250b96907dae7f88a07a
Pier Fumagalli <pier@usrz.com>
2023-09-04 15:59:05 +0200

    Proper types for request and response

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,22 @@
-// const string = 'INSERT INTO users(name, email) $$ VALUES($12, $2) RETURNING * $9'
-// type Q = Record<'2', string>
+import type { Result } from './connection'
 
-// const q: Q = [ 'a', 'b' ] as const
+export interface Request {
+  id: string,
+  query: string,
+  params?: any[],
+}
 
+export interface PositiveResponse extends Result {
+  id: string,
+  error?: never,
+}
 
-// type XXX<T extends string> = T extends N ? `Y[${T}]` : `N[${T}]`
-// type X = XXX<'1'>
+export type NegativeResponse = {
+  [ key in keyof Result ]?: never
+} & {
+  id: string,
+  error: string,
+}
 
-// type N = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
-
-// type SSS = typeof string
-// type S = S1<SSS>
-// type S1<SSS> =
-//     SSS extends `${string}\$${infer Rest}` ? S2<Rest> : never
-// type S2<Rest> =
-//     Rest extends `${N}${string}` ?
-//         Rest extends `${infer C1}${infer C2}${infer C3}${infer C4}` ?
-//             C3 extends `${N}` ? number :
-//             C2 extends `${N}` ? `${C1}${C2}` | S1<`${C3}${C4}`> :
-//             C1 extends `${N}` ? `${C1}` | S1<`${C2}${C3}${C4}`> :
-//             S1<Rest> :
-//             `[${Rest}]` :
-//         S1<Rest>
-
-// function foo<Q extends string, P extends readonly any[] & Record<S1<Q>, any>>(query: Q, ...params: P): void {
-//   void query, params
-// }
+export type Response = {
+} & ( PositiveResponse | NegativeResponse )
