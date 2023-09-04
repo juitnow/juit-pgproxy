bcac96cac2e18476da1f4ddc5fd32814e08e586e
Pier Fumagalli <pier@usrz.com>
2023-09-04 18:15:52 +0200

    Minimal `fetch`

--- test/utils.ts
+++ test/utils.ts
@@ -1,35 +1,39 @@
 import { createHmac, randomBytes } from 'node:crypto'
+import { request } from 'node:http'
 
 import { $gry, context } from '@plugjs/build'
 
 import type { Logger } from '../src/logger'
 
-const testLogs = process.env.TEST_LOGS === 'true'
 
+/** Our test logger */
 export class TestLogger implements Logger {
+  private _testLogs = process.env.TEST_LOGS === 'true'
   private _logger = context().log
 
   debug(...args: any[]): void {
-    if (testLogs) this._logger.info($gry('[dbg]'), ...args)
+    if (this._testLogs) this._logger.info($gry('[dbg]'), ...args)
   }
 
   info(...args: any[]): void {
-    if (testLogs) this._logger.notice($gry('[nfo]'), ...args)
+    if (this._testLogs) this._logger.notice($gry('[nfo]'), ...args)
   }
 
   warn(...args: any[]): void {
-    if (testLogs) this._logger.warn($gry('[wrn]'), ...args)
+    if (this._testLogs) this._logger.warn($gry('[wrn]'), ...args)
   }
 
   error(...args: any[]): void {
-    if (testLogs) this._logger.error($gry('[err]'), ...args)
+    if (this._testLogs) this._logger.error($gry('[err]'), ...args)
   }
 }
 
+/** Sleep for a few milliseconds */
 export function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
+/** Create our authentication token */
 export function createToken(secret: string): Buffer {
   const buffer = randomBytes(48)
 
@@ -41,4 +45,43 @@ export function createToken(secret: string): Buffer {
       .copy(buffer, 16)
 
   return buffer
+}
+
+/**
+ * Use a simplified `fetch`, the normal one leaves sockets around when
+ * connecting to localhost and _sometimes_ our tests don't end...
+ */
+export function fetch(url: URL, options: {
+  method?: 'POST' | 'GET',
+  headers?: Record<string, string>,
+  body?: any,
+  bodyRaw?: any,
+}): Promise<{ status: number, body: object }> {
+  return new Promise((resolve, reject) => {
+    const req = request(url, {
+      method: options.method || 'POST',
+      headers: {
+        'content-type': 'application/json',
+        ...options.headers,
+      },
+    }, (res) => {
+      const buffers: Buffer[] = []
+
+      res.on('data', (buffer) => buffers.push(buffer))
+      res.on('error', (error) => reject(error))
+      res.on('end', () => {
+        try {
+          const json = Buffer.concat(buffers).toString('utf-8')
+          const body = json ? JSON.parse(json) : undefined
+          return resolve({ status: res.statusCode || 500, body })
+        } catch (error) {
+          reject(error)
+        }
+      })
+    })
+
+    if (options.body) options.bodyRaw = JSON.stringify(options.body)
+    if (options.bodyRaw) req.write(options.bodyRaw, (err) => (err && reject(err)))
+    req.end()
+  })
 }
