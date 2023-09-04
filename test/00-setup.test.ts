f7e5e3876f4ba781bf2d2bcc7de57872a743e7f9
Pier Fumagalli <pier@usrz.com>
2023-09-04 19:44:32 +0200

    Why is node running?

--- test/00-setup.test.ts
+++ test/00-setup.test.ts
@@ -2,10 +2,36 @@ import { randomUUID } from 'node:crypto'
 
 import { $gry, $ylw } from '@plugjs/build'
 import pg from 'pg'
+// @ts-ignore
+import winr from 'why-is-node-running'
 
 export const databaseName = `test-${randomUUID()}`
 
+beforeAll(async () => {
+  log.notice(`Creating database ${$ylw(databaseName)}`)
+  const client = new pg.Client({ database: 'postgres' })
+  await client.connect()
+  try {
+    await client.query(`CREATE DATABASE "${databaseName}"`)
+  } finally {
+    await client.end()
+  }
+
+  const client2 = new pg.Client({ database: databaseName })
+  await client2.connect()
+  try {
+    await client2.query('CREATE TABLE "test" ("str" VARCHAR(32) NOT NULL, "num" INTEGER NOT NULL)')
+    await client2.query('INSERT INTO "test" ("str", "num") VALUES (\'foo\', 1), (\'bar\', 2), (\'baz\', 3)')
+  } finally {
+    await client2.end()
+  }
+})
+
 afterAll(async () => {
+  // we have some cases where the server won't exit... WHY? Let's use
+  // "why-is-node-running" 10 seconds after the database is destroyed
+  setTimeout(winr, 10_000).unref()
+
   log.notice(`\nDropping database ${$ylw(databaseName)}`)
   const mainClient = new pg.Client({ database: 'postgres' })
   await mainClient.connect()
@@ -40,25 +66,3 @@ afterAll(async () => {
 
   expect(leftovers, `Left ${leftovers} connections`).toStrictlyEqual(0)
 }, 30_000)
-
-beforeAll(async () => {
-  log.notice(`Creating database ${$ylw(databaseName)}`)
-  const client = new pg.Client({ database: 'postgres' })
-  await client.connect()
-  try {
-    await client.query(`CREATE DATABASE "${databaseName}"`)
-  } finally {
-    await client.end()
-  }
-
-  const client2 = new pg.Client({ database: databaseName })
-  await client2.connect()
-  try {
-    await client2.query('CREATE TABLE "test" ("str" VARCHAR(32) NOT NULL, "num" INTEGER NOT NULL)')
-    await client2.query('INSERT INTO "test" ("str", "num") VALUES (\'foo\', 1), (\'bar\', 2), (\'baz\', 3)')
-  } finally {
-    await client2.end()
-  }
-})
-
-// ?afterAll()
