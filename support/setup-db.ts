3653e1a21688544d41434e2af56e8261c60b9f76
Pier Fumagalli <pier@usrz.com>
2023-09-07 13:20:47 +0200

    Only use `LibPQ`

--- support/setup-db.ts
+++ support/setup-db.ts
@@ -1,62 +1,85 @@
 import { randomUUID } from 'node:crypto'
 
 import { $gry, $ylw } from '@plugjs/build'
-import pg from 'pg'
+import LibPQ from 'libpq'
 
 export const databaseName = `test-${randomUUID()}`
 
+// LibPQ "connectSync" fails with the following:
+// | Assertion failed: (!(handle->flags & UV_HANDLE_CLOSED)),
+// | function uv__finish_close, file core.c, line 271.
+// Assuming something is odd in "connection.cc" at around line 30, see
+// https://github.com/brianc/node-libpq/blob/master/src/connection.cc#L30
+function connect(pq: LibPQ, params: string): Promise<LibPQ> {
+  return new Promise((res, rej) => {
+    pq.connect(params, (err) => err ? rej(err) : res(pq))
+  })
+}
+
 beforeAll(async () => {
   log.notice(`Creating database ${$ylw(databaseName)}`)
-  const client = new pg.Client({ database: 'postgres' })
-  await client.connect()
-  try {
-    await client.query(`CREATE DATABASE "${databaseName}"`)
-  } finally {
-    await client.end()
-  }
 
-  const client2 = new pg.Client({ database: databaseName })
-  await client2.connect()
-  try {
-    await client2.query('CREATE TABLE "test" ("str" VARCHAR(32) NOT NULL, "num" INTEGER NOT NULL)')
-    await client2.query('INSERT INTO "test" ("str", "num") VALUES (\'foo\', 1), (\'bar\', 2), (\'baz\', 3)')
-  } finally {
-    await client2.end()
-  }
+  // create our test database
+  const pq = await connect(new LibPQ, 'dbname=postgres')
+
+  pq.execParams(`CREATE DATABASE "${databaseName}"`)
+  expect(pq.resultStatus(), pq.errorMessage()).toStrictlyEqual('PGRES_COMMAND_OK')
+
+  pq.finish()
+
+  // populate our test database with some test data
+  await connect(pq, `dbname=${databaseName} application_name=pool:beforeAll`)
+
+  pq.exec('CREATE TABLE "test" ("str" VARCHAR(32) NOT NULL, "num" INTEGER NOT NULL)')
+  expect(pq.resultStatus(), pq.errorMessage()).toStrictlyEqual('PGRES_COMMAND_OK')
+
+  pq.exec('INSERT INTO "test" ("str", "num") VALUES (\'foo\', 1), (\'bar\', 2), (\'baz\', 3)')
+  expect(pq.resultStatus(), pq.errorMessage()).toStrictlyEqual('PGRES_COMMAND_OK')
+
+  pq.finish()
 })
 
 afterAll(async () => {
   log.notice(`\nDropping database ${$ylw(databaseName)}`)
-  const mainClient = new pg.Client({ database: 'postgres' })
-  await mainClient.connect()
 
-  let leftovers = 0
+  const pq = await connect(new LibPQ(), 'dbname=postgres')
+  let connections = 0
   try {
-    // do we have any leftover connections?
-    const result = await mainClient.query(`
+    // Figure out if there are leftover connections...
+    pq.execParams(`
       SELECT application_name
         FROM pg_stat_activity
-       WHERE application_name LIKE 'pool:%'
-         AND datname=$1`, [ databaseName ])
+      WHERE application_name LIKE 'pool:%'
+        AND datname=$1`, [ databaseName ])
+    expect(pq.resultStatus(), pq.errorMessage()).toStrictlyEqual('PGRES_TUPLES_OK')
+    expect(pq.nfields(), 'Fields').toStrictlyEqual(1)
 
-    if (result.rows.length) {
-      log.warn('Found', $ylw(`${result.rows.length}`), 'connections left open:')
-      result.rows.forEach(({ application_name: name }) => log.warn($gry('  -'), $ylw(name)))
-      leftovers = result.rows.length
+    connections = pq.ntuples()
+    if (connections > 0) {
+      log.warn('Found', $ylw(`${connections}`), 'connections left open:')
+      for (let i = 0; i < connections; i ++) {
+        log.warn($gry('  -'), $ylw(pq.getvalue(i, 0)))
+      }
     }
 
-    // kill all remaining connections
-    await mainClient.query(`
+    // Kill any leftover connection (or at least try to)
+    pq.execParams(`
       SELECT pg_terminate_backend(pid)
         FROM pg_stat_activity
        WHERE application_name LIKE 'pool:%'
          AND pid <> pg_backend_pid()
          AND datname = $1`, [ databaseName ])
+    expect(pq.resultStatus(), pq.errorMessage()).toStrictlyEqual('PGRES_TUPLES_OK')
 
-    await mainClient.query(`DROP DATABASE "${databaseName}"`)
+    // Drop the database waiting at most 10 seconds
+    pq.exec('SET statement_timeout=10000')
+    expect(pq.resultStatus(), pq.errorMessage()).toStrictlyEqual('PGRES_COMMAND_OK')
+
+    pq.exec(`DROP DATABASE "${databaseName}"`)
+    expect(pq.resultStatus(), pq.errorMessage()).toStrictlyEqual('PGRES_COMMAND_OK')
   } finally {
-    await mainClient.end()
+    pq.finish()
   }
 
-  expect(leftovers, `Left ${leftovers} connections`).toStrictlyEqual(0)
+  expect(connections, `Left ${connections} connections`).toStrictlyEqual(0)
 }, 30_000)
