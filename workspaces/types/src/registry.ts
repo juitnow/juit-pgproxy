7ae75611dd339168329eecd6603cc6c3b3043de6
Pier Fumagalli <pier@usrz.com>
2023-09-08 20:38:43 +0200

    New OIDs type names

--- workspaces/types/src/registry.ts
+++ workspaces/types/src/registry.ts
@@ -1,4 +1,4 @@
-import { oids } from './oids'
+import { PGOIDs } from './oids'
 import {
   parseArray,
   parseBigInt,
@@ -11,9 +11,9 @@ import {
   parseCircle,
   parseCircleArray,
   parseFloatArray,
-  parseFloatRange,
   parseIntArray,
   parseIntRange,
+  parseIntRangeArray,
   parseInterval,
   parseIntervalArray,
   parseJson,
@@ -30,6 +30,7 @@ import {
   parseTimestampTzRange,
   parseVoid,
 } from './parsers'
+import { parseBigIntRangeArray, parseRangeArray, parseTimestampRangeArray, parseTimestampTzRangeArray } from './parsers/range'
 
 import type { PGParser } from './parsers'
 
@@ -42,70 +43,77 @@ export interface Registry {
 
 const defaultParsers: Record<number, PGParser<any>> = {
   /* Basic known types                                |_oid__|_typname______| */
-  [oids.bool]: parseBool, /*                          |   16 | bool         | */
-  [oids.bytea]: parseByteA, /*                        |   17 | bytea        | */
-  [oids.int8]: parseBigInt, /*                        |   20 | int8         | */
-  [oids.int2]: parseInt, /*                           |   21 | int2         | */
-  [oids.int4]: parseInt, /*                           |   23 | int4         | */
-  [oids.oid]: parseInt, /*                            |   26 | oid          | */
-  [oids.json]: parseJson, /*                          |  114 | json         | */
-  [oids.point]: parsePoint, /*                        |  600 | point        | */
-  [oids.float4]: parseFloat, /*                       |  700 | float4       | */
-  [oids.float8]: parseFloat, /*                       |  701 | float8       | */
-  [oids.circle]: parseCircle, /*                      |  718 | circle       | */
-  [oids.timestamp]: parseTimestamp, /*                | 1114 | timestamp    | */
-  [oids.timestamptz]: parseTimestampTz, /*            | 1184 | timestamptz  | */
-  [oids.interval]: parseInterval, /*                  | 1186 | interval     | */
-  [oids.jsonb]: parseJson, /*                         | 3802 | jsonb        | */
+  [PGOIDs.bool]: parseBool, /*                        |   16 | bool         | */
+  [PGOIDs.bytea]: parseByteA, /*                      |   17 | bytea        | */
+  [PGOIDs.int8]: parseBigInt, /*                      |   20 | int8         | */
+  [PGOIDs.int2]: parseInt, /*                         |   21 | int2         | */
+  [PGOIDs.int4]: parseInt, /*                         |   23 | int4         | */
+  [PGOIDs.oid]: parseInt, /*                          |   26 | oid          | */
+  [PGOIDs.json]: parseJson, /*                        |  114 | json         | */
+  [PGOIDs.point]: parsePoint, /*                      |  600 | point        | */
+  [PGOIDs.float4]: parseFloat, /*                     |  700 | float4       | */
+  [PGOIDs.float8]: parseFloat, /*                     |  701 | float8       | */
+  [PGOIDs.circle]: parseCircle, /*                    |  718 | circle       | */
+  [PGOIDs.timestamp]: parseTimestamp, /*              | 1114 | timestamp    | */
+  [PGOIDs.timestamptz]: parseTimestampTz, /*          | 1184 | timestamptz  | */
+  [PGOIDs.interval]: parseInterval, /*                | 1186 | interval     | */
+  [PGOIDs.jsonb]: parseJson, /*                       | 3802 | jsonb        | */
 
   /* Special types                                    |_oid__|_typname______| */
-  [oids.void]: parseVoid, /*                          | 2278 | void         | */
-  [oids.xid]: parseInt, /*                            |   28 | xid          | */
-  [oids.xid8]: parseBigInt, /*                        | 5069 | xid8         | */
-  [oids._xid]: parseIntArray, /*                      | 1011 | _xid         | */
-  [oids._xid8]: parseBigIntArray, /*                  |  271 | _xid8        | */
+  [PGOIDs.void]: parseVoid, /*                        | 2278 | void         | */
+  [PGOIDs.xid]: parseInt, /*                          |   28 | xid          | */
+  [PGOIDs.xid8]: parseBigInt, /*                      | 5069 | xid8         | */
+  [PGOIDs._xid]: parseIntArray, /*                    | 1011 | _xid         | */
+  [PGOIDs._xid8]: parseBigIntArray, /*                |  271 | _xid8        | */
 
   /* Native array types of the above                  |_oid__|_typname______| */
-  [oids._bool]: parseBoolArray, /*                    | 1000 | _bool        | */
-  [oids._bytea]: parseByteAArray, /*                  | 1001 | _bytea       | */
-  [oids._int8]: parseBigIntArray, /*                  | 1016 | _int8        | */
-  [oids._int2]: parseIntArray, /*                     | 1005 | _int2        | */
-  [oids._int4]: parseIntArray, /*                     | 1007 | _int4        | */
-  [oids._oid]: parseIntArray, /*                      | 1028 | _oid         | */
-  [oids._json]: parseJsonArray, /*                    |  199 | _json        | */
-  [oids._point]: parsePointArray, /*                  | 1017 | _point       | */
-  [oids._float4]: parseFloatArray, /*                 | 1021 | _float4      | */
-  [oids._float8]: parseFloatArray, /*                 | 1022 | _float8      | */
-  [oids._circle]: parseCircleArray, /*                |  719 | _circle      | */
-  [oids._timestamp]: parseTimestampArray, /*          | 1115 | _timestamp   | */
-  [oids._timestamptz]: parseTimestampTzArray, /*      | 1185 | _timestamptz | */
-  [oids._interval]: parseIntervalArray, /*            | 1187 | _interval    | */
-  [oids._jsonb]: parseJsonArray, /*                   | 3807 | _jsonb       | */
+  [PGOIDs._bool]: parseBoolArray, /*                  | 1000 | _bool        | */
+  [PGOIDs._bytea]: parseByteAArray, /*                | 1001 | _bytea       | */
+  [PGOIDs._int8]: parseBigIntArray, /*                | 1016 | _int8        | */
+  [PGOIDs._int2]: parseIntArray, /*                   | 1005 | _int2        | */
+  [PGOIDs._int4]: parseIntArray, /*                   | 1007 | _int4        | */
+  [PGOIDs._oid]: parseIntArray, /*                    | 1028 | _oid         | */
+  [PGOIDs._json]: parseJsonArray, /*                  |  199 | _json        | */
+  [PGOIDs._point]: parsePointArray, /*                | 1017 | _point       | */
+  [PGOIDs._float4]: parseFloatArray, /*               | 1021 | _float4      | */
+  [PGOIDs._float8]: parseFloatArray, /*               | 1022 | _float8      | */
+  [PGOIDs._circle]: parseCircleArray, /*              |  719 | _circle      | */
+  [PGOIDs._timestamp]: parseTimestampArray, /*        | 1115 | _timestamp   | */
+  [PGOIDs._timestamptz]: parseTimestampTzArray, /*    | 1185 | _timestamptz | */
+  [PGOIDs._interval]: parseIntervalArray, /*          | 1187 | _interval    | */
+  [PGOIDs._jsonb]: parseJsonArray, /*                 | 3807 | _jsonb       | */
 
   /* Other known array types                          |_oid__|_typname______| */
-  [oids._cidr]: parseArray, /*                        |  651 | _cidr        | */
-  [oids._money]: parseArray, /*                       |  791 | _money       | */
-  [oids._regproc]: parseArray, /*                     | 1008 | _regproc     | */
-  [oids._text]: parseArray, /*                        | 1009 | _text        | */
-  [oids._bpchar]: parseArray, /*                      | 1014 | _bpchar      | */
-  [oids._varchar]: parseArray, /*                     | 1015 | _varchar     | */
-  [oids._macaddr]: parseArray, /*                     | 1040 | _macaddr     | */
-  [oids._inet]: parseArray, /*                        | 1041 | _inet        | */
-  [oids._date]: parseArray, /*                        | 1182 | _date        | */
-  [oids._time]: parseArray, /*                        | 1183 | _time        | */
-  [oids._numeric]: parseArray, /*                     | 1231 | _numeric     | */
-  [oids._timetz]: parseArray, /*                      | 1270 | _timetz      | */
-  [oids._uuid]: parseArray, /*                        | 2951 | _uuid        | */
-  [oids._numrange]: parseArray, /*                    | 3907 | _numrange    | */
+  [PGOIDs._cidr]: parseArray, /*                      |  651 | _cidr        | */
+  [PGOIDs._money]: parseArray, /*                     |  791 | _money       | */
+  [PGOIDs._regproc]: parseArray, /*                   | 1008 | _regproc     | */
+  [PGOIDs._text]: parseArray, /*                      | 1009 | _text        | */
+  [PGOIDs._bpchar]: parseArray, /*                    | 1014 | _bpchar      | */
+  [PGOIDs._varchar]: parseArray, /*                   | 1015 | _varchar     | */
+  [PGOIDs._macaddr]: parseArray, /*                   | 1040 | _macaddr     | */
+  [PGOIDs._inet]: parseArray, /*                      | 1041 | _inet        | */
+  [PGOIDs._date]: parseArray, /*                      | 1182 | _date        | */
+  [PGOIDs._time]: parseArray, /*                      | 1183 | _time        | */
+  [PGOIDs._numeric]: parseArray, /*                   | 1231 | _numeric     | */
+  [PGOIDs._timetz]: parseArray, /*                    | 1270 | _timetz      | */
+  [PGOIDs._uuid]: parseArray, /*                      | 2951 | _uuid        | */
 
   /* Range types                                      |_oid__|_typname______| */
-  [oids.int4range]: parseIntRange, /*                 | 3904 | int4range    | */
-  [oids.numrange]: parseFloatRange, /*                | 3906 | numrange     | */
-  [oids.tsrange]: parseTimestampRange, /*             | 3908 | tsrange      | */
-  [oids.tstzrange]: parseTimestampTzRange, /*         | 3910 | tstzrange    | */
-  [oids.daterange]: parseRange, /*                    | 3912 | daterange    | */
-  [oids.int8range]: parseBigIntRange, /*              | 3926 | int8range    | */
-} // TODO satisfies Record<OIDs[keyof OIDs], PGParser<any>>
+  [PGOIDs.int4range]: parseIntRange, /*               | 3904 | int4range    | */
+  [PGOIDs.numrange]: parseRange, /*                   | 3906 | numrange     | */
+  [PGOIDs.tsrange]: parseTimestampRange, /*           | 3908 | tsrange      | */
+  [PGOIDs.tstzrange]: parseTimestampTzRange, /*       | 3910 | tstzrange    | */
+  [PGOIDs.daterange]: parseRange, /*                  | 3912 | daterange    | */
+  [PGOIDs.int8range]: parseBigIntRange, /*            | 3926 | int8range    | */
+
+  /* Array of range types                             |_oid__|_typname______| */
+  [PGOIDs._int4range]: parseIntRangeArray, /*         | 3905 | _int4range   | */
+  [PGOIDs._numrange]: parseRangeArray, /*             | 3907 | _numrange    | */
+  [PGOIDs._tsrange]: parseTimestampRangeArray, /*     | 3909 | _tsrange     | */
+  [PGOIDs._tstzrange]: parseTimestampTzRangeArray, /* | 3911 | _tstzrange   | */
+  [PGOIDs._daterange]: parseRangeArray, /*            | 3913 | _daterange   | */
+  [PGOIDs._int8range]: parseBigIntRangeArray, /*      | 3927 | _int8range   | */
+} satisfies Record<PGOIDs[keyof PGOIDs], PGParser<any>>
 
 class RegistryImpl implements Registry {
   private _parsers: Record<number, PGParser<any>>
