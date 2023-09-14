cecc4f37e2a15ab58cb5c82b338b89bf307f3c3f
Pier Fumagalli <pier@usrz.com>
2023-09-14 21:22:23 +0200

    Export serialization

--- workspaces/types/src/index.ts
+++ workspaces/types/src/index.ts
@@ -1,6 +1,7 @@
 export * from './oids'
 export * from './parsers'
 export * from './registry'
+export * from './serializers'
 
 /* Re-export our classes and constructors */
 export type { PGArray } from './parsers/array'
