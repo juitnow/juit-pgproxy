9082f7675c7e3e1d28fa48bd5a7cda8aa79e26c2
Pier Fumagalli <pier@usrz.com>
2023-10-04 23:13:56 +0200

    Cleanup, use model and shema

--- workspaces/utils/src/migrate.ts
+++ workspaces/utils/src/migrate.ts
@@ -4,7 +4,7 @@ import { basename } from 'node:path'
 import { Persister } from '@juit/pgproxy-persister'
 import { $blu, $grn, $gry, $ms, $und, $ylw, find, fs, log, merge, resolve } from '@plugjs/plug'
 
-import type { Schema } from '@juit/pgproxy-persister'
+import type { InferTableType, Schema } from '@juit/pgproxy-persister'
 
 /* ========================================================================== *
  * INTERNALS                                                                  *
@@ -12,35 +12,14 @@ import type { Schema } from '@juit/pgproxy-persister'
 
 const migrationsExpression = /^([0-9]+)[^\w](.*)\.(sql)$/i
 
-type MigrationFile = {
+type Migration = {
   sha256sum: Buffer,
   contents: string,
   number: number,
   name: string,
 }
 
-type xxStoredMigration = {
-  group: string,
-  number: number,
-  name: string,
-  sha256sum: Buffer,
-  timestamp: Date,
-}
-
-type Migration = {
-  group: string,
-  number: number,
-  name: string,
-  sha256sum: string,
-  timestamp: Date,
-}
-
-/* ========================================================================== *
- * EXPORTS                                                                    *
- * ========================================================================== */
-
-/** The persister schema for the `$migrations` table. */
-export const migrationSchema = {
+const migrationSchema = {
   '$migrations': {
     'group': { oid: 1043, isNullable: false, hasDefault: true },
     'number': { oid: 23, isNullable: false, hasDefault: false },
@@ -50,6 +29,9 @@ export const migrationSchema = {
   },
 } as const satisfies Schema
 
+/* ========================================================================== *
+ * EXPORTS                                                                    *
+ * ========================================================================== */
 
 export type MigrationOptions = {
   /** The directory where migrations SQL files reside (default: `./sql`) */
@@ -66,7 +48,7 @@ export type MigrationOptions = {
 export async function migrate(
     url: string | URL,
     options?: MigrationOptions,
-): Promise<Migration[]> {
+): Promise<number> {
   const {
     /* Default to our "../sql" migrations directory */
     migrations: migrationsDirectory = resolve('sql'),
@@ -110,21 +92,21 @@ export async function migrate(
 
   /* Filter unmatched migrations and sort them by migration _number_ */
   const migrationFiles = (await Promise.all(promises))
-      .filter((migration): migration is MigrationFile => !! migration)
+      .filter((migration): migration is Migration => !! migration)
       .sort((a, b) => a!.number - b!.number)
 
   /* Start our gigantic migrations transaction */
   const now = Date.now()
   const persister = new Persister(url, migrationSchema)
-  const migrations = persister.connect(async (client) => {
-    log.notice(`Applying ${$ylw(migrationFiles.length.toString())} migrations`)
+  return await persister.connect(async (connection) => {
+    const model = connection.in('$migrations')
 
     log.info('Beginning migrations transaction')
-    await client.begin()
+    await connection.begin()
 
     /* First of all, make sure we have our "$migrations" table */
     log.info(`Ensuring presence of ${$blu('$migrations')} table`)
-    await client.query(`
+    await connection.query(`
       SET LOCAL client_min_messages TO WARNING;
       CREATE TABLE IF NOT EXISTS "$migrations" (
         "group"     VARCHAR(32)  NOT NULL DEFAULT 'default',
@@ -137,65 +119,48 @@ export async function migrate(
 
     /* Lock our migrations table */
     log.info(`Lock exclusive use of ${$blu('$migrations')} table`)
-    await client.query('LOCK TABLE "$migrations"')
+    await connection.query('LOCK TABLE "$migrations"')
 
     /* Gather all applied migrations */
     log.info(`Looking for entries in ${$blu('$migrations')} table`)
-    const result = await client.query<xxStoredMigration>(`
-      SELECT "group",
-             "number",
-             "name",
-             "timestamp",
-             "sha256sum"
-        FROM "$migrations"
-       WHERE "group"=$1`, [ group ])
-    const applied = result.rows.reduce((applied, row) => {
+    const result = await model.read({ group })
+
+    /* Reduce all existing migration, keying them by number */
+    const applied = result.reduce((applied, row) => {
       const { group, number, name, timestamp, sha256sum } = row
       applied[number] = { group, number, name, timestamp, sha256sum }
       return applied
-    }, {} as Record<number, xxStoredMigration>)
+    }, {} as Record<number, InferTableType<typeof migrationSchema['$migrations']>>)
 
-    /** Apply our migrations and collect results */
-    const results: Migration[] = []
-
+    /* Apply our migrations */
+    let count = 0
     for (const { number, name, contents, sha256sum } of migrationFiles) {
       const num = `${number}`.padStart(3, '0')
-      if (applied[number]) {
-        const prev = applied[number]!
-
+      const prev = applied[number]
+      if (prev) {
         if (sha256sum.equals(prev.sha256sum)) {
           const timestamp = prev.timestamp.toISOString().substring(0, 19).replace('T', ' ')
           log.notice(`Skipping migration ${$gry(`${group}@`)}${$grn(num)}: ${$blu(name)}`, $gry(`applied on ${$und(timestamp)}`))
         } else {
+          log.warn(`Failed migration ${$gry(`${group}@`)}${$grn(num)}: ${$ylw(name)}`)
           const currHash = sha256sum.toString('hex').substring(0, 6)
-          const prevHash = prev.sha256sum.toString('hex').substring(0, 6)
+          const prevHash = Buffer.from(prev.sha256sum).toString('hex').substring(0, 6)
           throw new Error(`Migration ${group}@${num} (${name}) has checksum "${currHash}" but was recorded as "${prevHash}"`)
         }
       } else {
         log.notice(`Applying migration ${$gry(`${group}@`)}${$grn(num)}: ${$blu(name)}`)
-        await client.query(contents)
-        const result = await client.query<xxStoredMigration>(`
-          INSERT INTO "$migrations" ("group", "number", "name", "sha256sum")
-               VALUES ($1, $2, $3, $4)
-            RETURNING *`, [ group, number, name, sha256sum ])
-        results.push({
-          group: result.rows[0]!.group,
-          number: result.rows[0]!.number,
-          name: result.rows[0]!.name,
-          sha256sum: result.rows[0]!.sha256sum.toString('hex'),
-          timestamp: result.rows[0]!.timestamp,
-        })
+        await connection.query(contents)
+        await model.create({ group, number, name, sha256sum })
+        count ++
       }
     }
 
     /* Commit our migrations */
     log.info('Committing migrations transaction')
-    await client.commit()
+    await connection.commit()
 
     /* All done */
-    log.notice(`Applied ${$ylw(results.length.toString())} migrations ${$ms(Date.now() - now)}`)
-    return results
+    log.notice(`Applied ${$ylw(count)} migrations ${$ms(Date.now() - now)}`)
+    return count
   }).finally(() => persister.destroy())
-
-  return await migrations
 }
