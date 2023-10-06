dec2b449c3ff2c3d98f43124c0e0d0eeb722ad6b
Pier Fumagalli <pier@usrz.com>
2023-10-06 02:21:58 +0200

    Inital type tests

---
+++ test-d/01-persister-noschema.test-d.ts
@@ -0,0 +1,68 @@
+import { Persister } from '@juit/pgproxy-persister'
+import { expectType, printType } from 'tsd'
+
+import type { ColumnDefinition, Model } from '@juit/pgproxy-persister'
+
+printType('__file_marker__')
+
+const persister = new Persister()
+const model = persister.in('myTable')
+expectType<Model<Record<string, ColumnDefinition>>>(model)
+const connectionModel = await persister.connect((connection) => connection.in('myTable'))
+expectType<Model<Record<string, ColumnDefinition>>>(connectionModel)
+
+// ===== CREATE ================================================================
+
+expectType<(
+data: Record<string, any>
+) => Promise<Record<string, any>>>(model.create)
+
+// ===== UPSERT ================================================================
+
+/* with no keys */
+expectType<(
+keys: {},
+data: Omit<Record<string, any>, never>,
+) => Promise<Record<string, any>>>(model.upsert<{}>)
+
+expectType<Record<string, any>>(await model.upsert({}, {}))
+expectType<Record<string, any>>(await model.upsert({}, { myColumn: 1234 }))
+
+/* with a key */
+expectType<(
+keys: { myColumn: number },
+data: Omit<Record<string, any>, 'myColumn'>,
+) => Promise<Record<string, any>>>(model.upsert<{ myColumn: number }>)
+
+expectType<Record<string, any>>(await model.upsert({ myColumn: 1234 }, { anotherColumn: 'foo' }))
+// can not figure out a way to remove a specific from a Record<string, any>...
+expectType<Record<string, any>>(await model.upsert({ myColumn: 1234 }, { myColumn: 4321 }))
+
+// ===== READ ==================================================================
+
+expectType<(
+query?: Record<string, any>,
+sort?: string | string[],
+offset?: number,
+limit?: number,
+) => Promise<Record<string, any>[]>>(model.read)
+
+// ===== FIND ==================================================================
+
+expectType<(
+query?: Record<string, any>,
+sort?: string | string[],
+) => Promise<Record<string, any> | undefined>>(model.find)
+
+// ===== UPDATE ================================================================
+
+expectType<(
+query: Record<string, any>,
+patch: Record<string, any>,
+) => Promise<Record<string, any>[]>>(model.update)
+
+// ===== DELETE ================================================================
+
+expectType<(
+query: Record<string, any>,
+) => Promise<number>>(model.delete)
