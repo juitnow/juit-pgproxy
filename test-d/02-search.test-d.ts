a3fe83defaa0b259f6485535f0da440ba571fcd6
Pier Fumagalli <pier@usrz.com>
2025-11-21 20:26:24 +0100

    Test search types

---
+++ test-d/02-search.test-d.ts
@@ -0,0 +1,171 @@
+import { expectType, printType } from 'tsd'
+
+import type { SearchOptions, SearchResult } from '../workspaces/model/src/index'
+
+printType('__file_marker__')
+
+interface TestSchema {
+  'joined': {
+    'uuid': {
+      type: string
+      hasDefault: true
+    }
+    'key': {
+      type: string
+    }
+    'date': {
+      type: Date
+      isNullable: true
+    }
+    'json': {
+      type: any
+      isNullable: true
+    }
+  }
+  'main': {
+    'uuid': {
+      type: string
+      hasDefault: true
+    }
+    'ref': {
+      type: string
+      isNullable: true
+    }
+    'key': {
+      type: string
+    }
+    'date': {
+      type: Date
+    }
+    'number': {
+      type: number
+      isNullable: true
+    }
+    'json': {
+      type: any
+      isNullable: true
+    }
+  }
+}
+
+type TestJoins = {
+  referenced: { column: 'ref', refTable: 'joined', refColumn: 'uuid', sortColumn: 'key' },
+}
+
+type TestJoins2 = {
+  referenced: { column: 'key', refTable: 'joined', refColumn: 'key' },
+}
+
+/* ===== SEARCH OPTIONS ===================================================== */
+
+// With joins
+expectType<{
+  limit?: number | undefined,
+  offset?: number | undefined,
+  sort?: 'uuid' | 'ref' | 'key' | 'date' | 'number' | 'json' // from the main table
+       | 'referenced' // joined table sort column
+       | undefined,
+  order?: 'asc' | 'desc' | undefined,
+  q?: string | undefined,
+  filters?: ({
+    name: 'uuid' | 'ref' | 'key' | 'date' | 'number' | 'json',
+    field?: string | undefined,
+    op?: '=' | '!=' | '>' | '>=' | '<' | '<=' | '~' | 'like' | 'ilike'
+    value: string | number | Date | boolean | null
+  } | {
+    name: 'uuid' | 'ref' | 'key' | 'date' | 'number' | 'json',
+    field?: string | undefined,
+    op: 'in' | 'not in',
+    value: (string | number | Date | boolean | null)[]
+  } | {
+    name: 'uuid' | 'ref' | 'key' | 'date' | 'number' | 'json',
+    field?: never,
+    op: '@>' | '<@',
+    value: any
+  })[] | undefined
+}>(null as any as SearchOptions<TestSchema, 'main', TestJoins>)
+
+// With non-sortable joins
+expectType<{
+  limit?: number | undefined,
+  offset?: number | undefined,
+  sort?: 'uuid' | 'ref' | 'key' | 'date' | 'number' | 'json' // from the main table
+       | undefined, // no 'referenced' here... it's not sortable!
+  order?: 'asc' | 'desc' | undefined,
+  q?: string | undefined,
+  filters?: ({
+    name: 'uuid' | 'ref' | 'key' | 'date' | 'number' | 'json',
+    field?: string | undefined,
+    op?: '=' | '!=' | '>' | '>=' | '<' | '<=' | '~' | 'like' | 'ilike'
+    value: string | number | Date | boolean | null
+  } | {
+    name: 'uuid' | 'ref' | 'key' | 'date' | 'number' | 'json',
+    field?: string | undefined,
+    op: 'in' | 'not in',
+    value: (string | number | Date | boolean | null)[]
+  } | {
+    name: 'uuid' | 'ref' | 'key' | 'date' | 'number' | 'json',
+    field?: never,
+    op: '@>' | '<@',
+    value: any
+  })[] | undefined
+}>(null as any as SearchOptions<TestSchema, 'main', TestJoins2>)
+
+// With joins
+expectType<{
+  limit?: number | undefined,
+  offset?: number | undefined,
+  sort?: 'uuid' | 'ref' | 'key' | 'date' | 'number' | 'json' | undefined,
+  order?: 'asc' | 'desc' | undefined,
+  q?: string | undefined,
+  filters?: ({
+    name: 'uuid' | 'ref' | 'key' | 'date' | 'number' | 'json',
+    field?: string | undefined,
+    op?: '=' | '!=' | '>' | '>=' | '<' | '<=' | '~' | 'like' | 'ilike'
+    value: string | number | Date | boolean | null
+  } | {
+    name: 'uuid' | 'ref' | 'key' | 'date' | 'number' | 'json',
+    field?: string | undefined,
+    op: 'in' | 'not in',
+    value: (string | number | Date | boolean | null)[]
+  } | {
+    name: 'uuid' | 'ref' | 'key' | 'date' | 'number' | 'json',
+    field?: never,
+    op: '@>' | '<@',
+    value: any
+  })[] | undefined
+}>(null as any as SearchOptions<TestSchema, 'main', {}>)
+
+/* ===== SEARCH RESULTS ===================================================== */
+
+// With nullable joins
+expectType<{
+  uuid: string;
+  ref: string | null;
+  key: string;
+  date: Date;
+  number: number | null;
+  json: any | null;
+  referenced: null | {
+    uuid: string;
+    key: string;
+    date: Date | null;
+    json: any | null;
+  }
+}>(null as any as SearchResult<TestSchema, 'main', TestJoins>)
+
+// With non-nullable joins
+expectType<{
+  uuid: string;
+  ref: string | null;
+  key: string;
+  date: Date;
+  number: number | null;
+  json: any | null;
+  referenced: {
+    uuid: string;
+    key: string;
+    date: Date | null;
+    json: any | null;
+  }
+}>(null as any as SearchResult<TestSchema, 'main', TestJoins2>)
