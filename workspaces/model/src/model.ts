73e84b5967b69bf9f48b443d6eac33addfba7892
Pier Fumagalli <pier@usrz.com>
2025-11-21 21:29:51 +0100

    Prettify only where needed

--- workspaces/model/src/model.ts
+++ workspaces/model/src/model.ts
@@ -17,63 +17,60 @@ export interface ColumnDefinition<T = any> {
 }
 
 /** Infer the TypeScript type suitable for an `INSERT` in a table */
-export type InferInsertType<Table extends Record<string, ColumnDefinition>> =
-  Prettify<{
-    /* First part: all nullable or defaulted columns are optional */
-    [ Column in keyof Table as Column extends string
-      ? Table[Column]['isGenerated'] extends true ? never
-      : Table[Column]['isNullable'] extends true ? Column
-      : Table[Column]['hasDefault'] extends true ? Column
-      : never
-      : never
-    ] ? :
-    Table[Column]['isNullable'] extends true
-      ? Table[Column]['type'] | null
-      : Table[Column]['type']
-  } & {
-    /* Second part: all non-nullable or non-defaulted columns are required */
-    [ Column in keyof Table as Column extends string
-      ? Table[Column]['isGenerated'] extends true ? never
-      : Table[Column]['isNullable'] extends true ? never
-      : Table[Column]['hasDefault'] extends true ? never
-      : Column
-      : never
-    ] -? :
-    Table[Column]['isNullable'] extends true
-      ? Table[Column]['type'] | null
-      : Table[Column]['type']
-  }>
+export type InferInsertType<Table extends Record<string, ColumnDefinition>> = Prettify<{
+  /* First part: all nullable or defaulted columns are optional */
+  [ Column in keyof Table as Column extends string
+    ? Table[Column]['isGenerated'] extends true ? never
+    : Table[Column]['isNullable'] extends true ? Column
+    : Table[Column]['hasDefault'] extends true ? Column
+    : never
+    : never
+  ] ? :
+  Table[Column]['isNullable'] extends true
+    ? Table[Column]['type'] | null
+    : Table[Column]['type']
+} & {
+  /* Second part: all non-nullable or non-defaulted columns are required */
+  [ Column in keyof Table as Column extends string
+    ? Table[Column]['isGenerated'] extends true ? never
+    : Table[Column]['isNullable'] extends true ? never
+    : Table[Column]['hasDefault'] extends true ? never
+    : Column
+    : never
+  ] -? :
+  Table[Column]['isNullable'] extends true
+    ? Table[Column]['type'] | null
+    : Table[Column]['type']
+}>
 
 /** Infer the TypeScript type suitable for a `SELECT` from a table */
-export type InferSelectType<Table extends Record<string, ColumnDefinition>> =
-  Prettify<{
-    [ Column in keyof Table as Column extends string ? Column : never ] -? :
-      ( Table[Column]['isNullable'] extends true ?
-        Table[Column]['type'] | null :
-        Table[Column]['type']
-      ) & ( Table[Column] extends { branding: infer Brand } ? Brand : unknown )
-  }>
+export type InferSelectType<Table extends Record<string, ColumnDefinition>> = {
+  [ Column in keyof Table as Column extends string ? Column : never ] -? :
+    ( Table[Column]['isNullable'] extends true
+      ? Table[Column]['type'] | null
+      : Table[Column]['type']
+    ) & ( Table[Column] extends { branding: infer Brand } ? Brand : unknown )
+}
 
 /** Infer the TypeScript type suitable for a `UPDATE` in a table */
-export type InferUpdateType<Table extends Record<string, ColumnDefinition>> =
-  Prettify<{
-    [ Column in keyof Table as Column extends string
-      ? Table[Column]['isGenerated'] extends true ? never
-      : Column
-      : never
-    ] ? :
-    Table[Column]['isNullable'] extends true ?
-      Table[Column]['type'] | null :
-      Table[Column]['type']
-  }>
+export type InferUpdateType<Table extends Record<string, ColumnDefinition>> = {
+  [ Column in keyof Table as Column extends string
+    ? Table[Column]['isGenerated'] extends true ? never
+    : Column
+    : never
+  ] ? :
+  Table[Column]['isNullable'] extends true
+    ? Table[Column]['type'] | null
+    : Table[Column]['type']
+}
 
 /** Infer the TypeScript type used for querying records */
-export type InferQueryType<Table extends Record<string, ColumnDefinition>> =
-  Prettify<{ [ Column in keyof Table as Column extends string ? Column : never ] ? :
-    Table[Column]['isNullable'] extends true ?
-        Table[Column]['type'] | null :
-      Table[Column]['type']
-  }>
+export type InferQueryType<Table extends Record<string, ColumnDefinition>> ={
+  [ Column in keyof Table as Column extends string ? Column : never ] ? :
+  Table[Column]['isNullable'] extends true
+    ? Table[Column]['type'] | null
+    : Table[Column]['type']
+}
 
 /** Infer the available sort values for a table (as required by `ORDER BY`) */
 export type InferSort<Table extends Record<string, ColumnDefinition>> =
